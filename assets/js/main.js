623f15b79be724dfce0ab043fa559663773ff2a1
Pysched <48335713+Pysched@users.noreply.github.com>
2020-02-28 17:48:04 +0000

    Updated main.js
    Added commentary

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 // Function that takes the users inputted name value and displays it in a span within the game and also transitions the game from the landing page to the game page
 function getPlayerName(){
     document.getElementById("gamerId").innerHTML = document.getElementById('playerName').value + "";
-
     document.getElementById('landing-page').style.display = "none"; document.getElementById('game-page').style.display="flex";
 }
 
@@ -97,21 +96,24 @@ class SnapCardGame {
 
     }
 
-  
-
-
-
     // card flippping function
     flipCard(card){
+        // check first if the card can be flipped by calling the canCardFlip functon
         if(this.canCardFlip(card)){
+            // If it can then play the audio sound for it
             this.audioController.flip();
+            // Increment the totalclicks counter
             this.totalClicks++;
+            // Change the counter to update the clicks made
             this.counter.innerText = this.totalClicks;
+            // change the cards visiblility to visible
             card.classList.add('visible');
-
+        // If the card is able to be check i.e it is not null,
         if(this.cardCheck)
+        // Check for a match
             this.cardMatchCheck(card);
         else
+      // otherwise null = card
             this.cardCheck = card;
         }
 
@@ -119,24 +121,28 @@ class SnapCardGame {
 
     // Do the cards match
     cardMatchCheck(card){
+        // if cardtype(which card has been clicked) is equal to 
         if(this.cardtype(card) === this.cardtype(this.cardCheck))
             this.cardMatch(card, this.cardCheck);
         else    
             this.cardMisMatch(card, this.cardCheck);
             this.cardCheck = null;
     }
-
+    // Check the card type by getting the class of card actual and checking which card it is for further match checking
     cardtype(card){
         return card.getElementsByClassName('card-actual')[0].src;
     }
-    // cards match
+    // cards match - assign 2 parameters card 1 and card 2
     cardMatch(card1, card2){
+        // push first card to varable card 1 and same to card 2
         this.matchedCards.push(card1);
         this.matchedCards.push(card2);
+        // add a class of matched to each 
         card1.classList.add('matched');
         card2.classList.add('matched');
+        //play the audio of cards matched
         this.audioController.matched();
-
+        //if all matched cards equals the amount of cards, then the game is won and play the winning music
         if(this.matchedCards.length === this.cardsArray.length)
             this.winning();
     }
@@ -161,8 +167,11 @@ class SnapCardGame {
     }
     // gameover conditions
     gameOver(){
+        // Clear the countdown timer
         clearInterval(this.countDown);
+        // Play the audio instance of game over
         this.audioController.gameOver();
+        // Add a class of visible to the id of out-of-time
         document.getElementById('out-of-time').classList.add('visible');
         
     }
@@ -170,12 +179,16 @@ class SnapCardGame {
    
     // winning the game conditions
     winning(){
+        // Clear the countdown time
         clearInterval(this.countDown);
+        // Play the audio file for success
         this.audioController.winning();
+        // Add a class of visible to the id of winner
         document.getElementById('winner').classList.add('visible');
+        // Take the value of totalClicks and replace the inner html of the id total moves with that value 
         document.getElementById('totalMoves').innerHTML = this.totalClicks;
+        // Take the starting time -> totaltime value and subtrack it from the time remaining value and replace the id of timetaken with its result
         document.getElementById('timeTaken').innerHTML = this.totalTime - this.timeRemaining;
-        
     }
     // Card Shuffle function --- fisher Yeates Function for shuffling
     shuffleCards(){
@@ -190,13 +203,13 @@ class SnapCardGame {
     canCardFlip(card){
         return !this.selected && !this.matchedCards.includes(card) && card !== this.cardCheck;
     }
-
+    //This functions logic is = return true -> if not this.selected and not This.matchedcards and  card is not this.cardcheck to enable card to be flipped
 }
 
 
 
 
-// Function that declares overlay screens and card deck and adds visibiliy toggle and sets up startGame funciton and flipCard function
+//This function creates arrays for overlays and cards, declares the game through calling a new instance of the Javascript class name SnapCardGame and assigns it two parameters, 60 seconds and the array cards. I then adds an event listener for each instance of click on the overlays and removes their visiblity with this. It then runs the startGame Function.
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlays'));
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -208,6 +221,7 @@ function ready() {
         });
 
     });
+    // adds event listeners for each card and runs the funciton flipCard at each click of a card
         cards.forEach(card => {
             card.addEventListener('click', () => {
             game.flipCard(card);
